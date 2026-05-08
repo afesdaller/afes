927fba9d8a922d895d4daf1b63b0e04ef2a7d3b1
Afes <afesdaller@gmail.com>
2026-05-08 09:58:50 +0300

    fix photo frame

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -36,13 +36,13 @@ export default function Home() {
           unoptimized
         />
       )}
-      <div className="relative flex justify-center">
+      <div className="relative flex aspect-square w-[85%] max-w-[300px] items-center justify-center min-[840px]:w-[350px] min-[840px]:max-w-[350px]">
         <Image
           src="/page/photo frame.png"
           alt="photo frame"
           width={500}
           height={500}
-          className="relative z-1 h-auto w-[85%] max-w-[300px] rounded-full drop-shadow-[0_0_20px_rgba(0,0,0,0.5)] min-[840px]:w-[350px]"
+          className="relative z-1 h-full w-full rounded-full object-contain drop-shadow-[0_0_20px_rgba(0,0,0,0.5)]"
           priority
         />
         <Image
@@ -50,7 +50,7 @@ export default function Home() {
           alt="photo frame"
           width={230}
           height={230}
-          className="absolute top-[16%] left-1/2 z-0 h-auto w-[60%] max-w-[200px] -translate-x-1/2 rounded-full drop-shadow-[0_0_10px_rgba(0,0,0,0.5)] backdrop-blur-[5px] min-[840px]:top-[62px] min-[840px]:w-[230px]"
+          className="absolute top-[26%] left-1/2 z-0 aspect-square h-auto w-[60%] max-w-[200px] -translate-x-1/2 rounded-full object-contain drop-shadow-[0_0_10px_rgba(0,0,0,0.5)] backdrop-blur-[5px] min-[840px]:top-[62px] min-[840px]:w-[230px] min-[840px]:max-w-[230px]"
           priority
         />
       </div>
